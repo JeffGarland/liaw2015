235cfba256174628891b74cb96960389e2ddfa33
Richard Powell <richard.powell@apple.com>
2015-05-13 13:18:16 -0600

    a starting point

---
+++ include/boost/confoost/confoost.hpp
@@ -0,0 +1,16 @@
+/**
+ *  \file confoost.hpp
+ *
+ *  Copyright 2015 Jeff Garland
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef BOOST_CONFOOST_CONFOOST_HPP
+#define BOOST_CONFOOST_CONFOOST_HPP
+
+
+#endif // BOOST_CONFOOST_CONFOOST_HPP
+
+
